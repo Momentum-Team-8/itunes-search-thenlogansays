b23fadc9c966b744ae707c11fd28e1acab8cf6dd
loganatkinson <logan.atkinson@gmail.com>
2021-06-03 08:30:39 -0400

    got artist name to render to page

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 
 
-const url = 'https://proxy-itunes-api.glitch.me/search?'
+const url = 'https://itunes.apple.com/search?'
 const form = document.getElementById('input-field')
-const searchInput = document.getElementById('search-input').value
+const searchInput = document.getElementById('search-input')
 searchResults = document.getElementById('output')
 
 document.addEventListener('submit', event => {
     event.preventDefault()
-    createNewSearch()
+    createNewSearch(searchInput)
 });
 
 function createNewSearch() {
+    const search = searchInput.value
     //making request to the url for the search
-    fetch(url + 'term=' + `${searchInput}` + '&media=music' + '&entity=song' + '&limit=10')
+    fetch(url + 'term=' + `${search}` + '&entity=song' + '&limit=10')
     //returns the request in json format
     .then(response =>  response.json())
     //looping through data
     .then((data) => {
-        console.log(data)
-        console.log(url + 'term=' + `${searchInput}` + '&media=music' + '&limit=10')
+        //console.log(data)
+        //console.log(url + 'term=' + `${searchInput}` + '&media=music' + '&limit=10')
+        
         for (let item of data.results) {
             console.log(item)
             renderResults(item)
@@ -26,12 +28,13 @@ function createNewSearch() {
     })
 };
 
-function renderResults(musicObj) {
+function renderResults(data) {
     let eachResult = document.createElement('li')
+    eachResult.id = data.trackId
     searchResults.appendChild(eachResult)
 
     let artist = document.createElement('div')
     artist.setAttribute('id', 'artist-name')
     eachResult.appendChild(artist)
-    artist.innerHTML = `${musicObj.artistName}`
+    artist.innerHTML = `${data.artistName}`
 }
